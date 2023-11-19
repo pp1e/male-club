60bc6164e2c87bd6ac70f16eb82a5a88b5dda0a1
Polina Guseva (U-course) <pn_guseva@mail.ru>
2023-11-19 11:15:36 +0300

    upcoming-events

--- web_client/src/App.tsx
+++ web_client/src/App.tsx
@@ -4,6 +4,7 @@ import AdminPanel from "./components/AdminPanel/AdminPanel";
 import RegistrationPage from "./components/RegistrationPage/RegistrationPage";
 import NotFoundPage from "./components/NotFoundPage/NotFoundPage";
 import LoginPage from "./components/LoginPage/LoginPage";
+import EventsPage from "./components/EventsPage/EventsPage";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Consoles from "./components/Consoles/Consoles";
@@ -21,7 +22,8 @@ function App() {
         {/* <Route path="*" element={<PageOne />} /> */}
         <Route path="admin" element={<AdminPanel />} />
         <Route path="registration" element={<RegistrationPage />} />
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path="*" element={<NotFoundPage />} />       
+        <Route path="upcoming-events" element={<EventsPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="consoles" element={<Consoles />} />
       </Routes>
