c56f45b5d8eeca42270db11c86212241ee5cb755
Egor <91369300+Egorchick4476@users.noreply.github.com>
2023-11-19 21:09:23 +0300

    Update V011__Create_delete_child_procedure.sql

--- server/src/main/resources/db/migration/V011__Create_delete_child_procedure.sql
+++ server/src/main/resources/db/migration/V011__Create_delete_child_procedure.sql
@@ -2,6 +2,6 @@ CREATE PROCEDURE DeleteChild (
     IN child_id BIGINT
 )
 BEGIN
-    DELETE FROM reservation_console WHERE child_id = reservation_console.child_id;
+    DELETE FROM reservation WHERE child_id = reservation.child_id;
     DELETE FROM child WHERE id = child_id;
 END
