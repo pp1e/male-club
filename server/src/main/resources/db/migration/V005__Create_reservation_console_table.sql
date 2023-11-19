837b74dc218382d130e4ec732c458d7ac99dff4d
Egor <91369300+Egorchick4476@users.noreply.github.com>
2023-11-19 21:06:30 +0300

    Update V005__Create_reservation_console_table.sql

--- server/src/main/resources/db/migration/V005__Create_reservation_console_table.sql
+++ server/src/main/resources/db/migration/V005__Create_reservation_console_table.sql
@@ -1,8 +1,9 @@
-CREATE TABLE reservation_console (
+CREATE TABLE reservation (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     child_id BIGINT NOT NULL,
     console_id BIGINT NULL,
     time_and_date DATETIME NOT NULL,
+    phone VARCHAR(12) NOT NULL,
     CONSTRAINT relation_5_child_fk FOREIGN KEY (child_id) REFERENCES child (id),
     CONSTRAINT relation_5_console_fk FOREIGN KEY (console_id) REFERENCES console (id)
 );
