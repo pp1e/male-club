5a0d552d2595b09f4cd811d9cb8a16e80bd99946
Denis <denis.malyutin@fruct.org>
2023-11-19 20:45:43 +0300

    Add ReservationConsole 'get' API

--- server/src/main/kotlin/telepuziki/maleclub/controller/ReservationConsoleController.kt
+++ server/src/main/kotlin/telepuziki/maleclub/controller/ReservationConsoleController.kt
@@ -1,6 +1,7 @@
 package telepuziki.maleclub.controller
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.web.bind.annotation.*
 import telepuziki.maleclub.model.ReservationConsole
 import telepuziki.maleclub.repository.ReservationConsoleRepository
@@ -12,6 +13,11 @@ class ReservationConsoleController(@Autowired val reservationConsoleRepository: 
     @GetMapping("/list")
     fun getAllReservations(): List<ReservationConsole> {
         return reservationConsoleRepository.findAll()
+    }
+
+    @GetMapping("/get")
+    fun getChildrenById(@RequestParam id: Long): ReservationConsole? {
+        return reservationConsoleRepository.findByIdOrNull(id)
     }
 
     @PostMapping("/add")
