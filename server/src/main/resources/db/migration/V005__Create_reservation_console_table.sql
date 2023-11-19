c8d59b500ce5c51d6e3adbb3053ec56a95ea85a3
Egor <91369300+Egorchick4476@users.noreply.github.com>
2023-11-19 17:46:28 +0300

    Поправка в создании базы резервирования места

--- server/src/main/resources/db/migration/V005__Create_reservation_console_table.sql
+++ server/src/main/resources/db/migration/V005__Create_reservation_console_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE reservation_console (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     child_id BIGINT NOT NULL,
-    console_id BIGINT NOT NULL,
+    console_id BIGINT NULL,
     time_and_date DATETIME NOT NULL,
-    PRIMARY KEY (child_id, console_id),
     CONSTRAINT relation_5_child_fk FOREIGN KEY (child_id) REFERENCES child (id),
     CONSTRAINT relation_5_console_fk FOREIGN KEY (console_id) REFERENCES console (id)
 );
