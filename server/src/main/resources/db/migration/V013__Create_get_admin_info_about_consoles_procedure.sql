7f5b80921dfc3a1e9f9ffc9b9d4209f1e3d7c596
Egor <91369300+Egorchick4476@users.noreply.github.com>
2023-11-19 21:10:51 +0300

    Update V013__Create_get_admin_info_about_consoles_procedure.sql

--- server/src/main/resources/db/migration/V013__Create_get_admin_info_about_consoles_procedure.sql
+++ server/src/main/resources/db/migration/V013__Create_get_admin_info_about_consoles_procedure.sql
@@ -3,8 +3,8 @@ CREATE PROCEDURE GetAdminInfoAboutConsoles (
 )
 BEGIN
     SELECT child.firstname, console.Number, child.phone, child.count_visit, child.peculiarities
-    FROM reservation_console
-             INNER JOIN child ON child.id = reservation_console.child_id
-             INNER JOIN console ON console.id = reservation_console.console_id
-    WHERE reservation_console.time_and_date = date;
+    FROM reservation
+             INNER JOIN child ON child.id = reservation.child_id
+             INNER JOIN console ON console.id = reservation.console_id
+    WHERE reservation.time_and_date = date;
 END
