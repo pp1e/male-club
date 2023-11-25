8925f167a4dffb8bcf48a6e0e10e170b3eef9ef7
Polina Guseva (U-course) <pn_guseva@mail.ru>
2023-11-25 16:48:46 +0300

    404 + change values on relative

--- web_client/src/components/EventsPage/EventsPage.tsx
+++ web_client/src/components/EventsPage/EventsPage.tsx
@@ -1,6 +1,6 @@
 import { ReactElement, useState, useMemo } from "react";
 import NavBar from "../Navigation/NavBar";
-import { AUTHOR_IMAGE } from './Images';
+import { AUTHOR_IMAGE } from '../../resources/Images';
 import Image from 'react-bootstrap/Image';
 import "./styles/eventspage.css";
 
@@ -18,16 +18,13 @@ interface IProps {}
 
 const ChildCard = (props: IChildCard): ReactElement => { 
     return <>
-            <div className="events-page__card__container d-flex flex-column justify-content-center align-items-center mx-4 mb-5">
-                    <div className="events-page__card__image mb-4  d-flex flex-column justify-content-center align-items-center">
+            <div className="events-page__card__container d-flex flex-column justify-content-center align-items-center">
+                    <div className="events-page__card__image d-flex flex-column justify-content-center align-items-center">
                         <Image
                             src={AUTHOR_IMAGE}
-                            className=""
-                            width="120"
-                            height="135"
+                            className="aa"
                         />
-                    </div>
-                    
+                    </div>                    
                     <div className="d-flex flex-column events-page__text-container justify-content-between card__text-second">
                         <span className="card__text-main">{props.user.name}</span>
                         <span>{props.user.date}</span>
@@ -64,11 +61,11 @@ const EventsPage = (props: IProps): ReactElement => {
             date: "11.06.22",
             numPs: 14008
         },
-        {
-            name: "Денис",
-            date: "11.06.22",
-            numPs: 14008
-        },
+        // {
+        //     name: "Денис",
+        //     date: "11.06.22",
+        //     numPs: 14008
+        // },
     ]);
     const childrenList = useMemo(() => {
         if (userList.length > 0) {
@@ -85,9 +82,9 @@ const EventsPage = (props: IProps): ReactElement => {
     return (
         <>
             <NavBar />
-            <div className="container py-2 d-flex flex-column align-items-center font-weight-normal text-center">
+            <div className="container d-flex flex-column align-items-center font-weight-normal text-center">
                 <span className="events-page__text-main">Предстоящие записи</span>
-                <div className="d-flex flex-row justify-content-start flex-wrap events-page__container">
+                <div className="d-flex flex-row justify-content-center flex-wrap events-page__container">
                     {childrenList}
                 </div>
             </div>
