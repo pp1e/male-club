e5822681d880b1fe26e86c4618b00c4d3f20fb6d
Egor <91369300+Egorchick4476@users.noreply.github.com>
2023-11-26 17:04:37 +0300

    Update V003__Create_child_table.sql

--- server/src/main/resources/db/migration/V003__Create_child_table.sql
+++ server/src/main/resources/db/migration/V003__Create_child_table.sql
@@ -2,7 +2,6 @@ CREATE TABLE child(
     id BIGINT NOT NULL AUTO_INCREMENT,
     firstname VARCHAR(50) NOT NULL,
     peculiarities VARCHAR(200) NULL,
-    phone VARCHAR(12) NOT NULL,
     count_visit INT NOT NULL,
     user_id BIGINT NOT NULL,
     birthdate DATE NOT NULL,
