e5f4762caaae3c0e6ba26c59032417b6b06de2c6
Denis <denis.malyutin@fruct.org>
2024-01-09 15:38:42 +0300

    Fix not listing events without consoles

--- server/src/main/resources/db/migration/production/V007__Create_get_upcoming_events_procedure.sql
+++ server/src/main/resources/db/migration/production/V007__Create_get_upcoming_events_procedure.sql
@@ -5,7 +5,7 @@ BEGIN
     SELECT reservation.id, child.firstname, reservation.time_and_date, console.Number
     FROM child
         INNER JOIN reservation ON child.id = reservation.child_id
-        INNER JOIN console ON reservation.console_id = console.id
+        LEFT JOIN console ON reservation.console_id = console.id
     WHERE child.user_id = parent_id AND reservation.time_and_date > NOW()
     ORDER BY reservation.time_and_date;
 END
