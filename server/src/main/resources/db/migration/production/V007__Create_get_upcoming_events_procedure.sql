c8f3e74ca8aca1ecd1e964528fb45d7934ed820c
Denis <denis.malyutin@fruct.org>
2024-01-09 17:21:30 +0300

    Fix listing confirmed reservations for user

--- server/src/main/resources/db/migration/production/V007__Create_get_upcoming_events_procedure.sql
+++ server/src/main/resources/db/migration/production/V007__Create_get_upcoming_events_procedure.sql
@@ -6,6 +6,6 @@ BEGIN
     FROM child
         INNER JOIN reservation ON child.id = reservation.child_id
         LEFT JOIN console ON reservation.console_id = console.id
-    WHERE child.user_id = parent_id AND reservation.time_and_date > NOW()
+    WHERE child.user_id = parent_id AND reservation.time_and_date > NOW() AND is_confirmed = false
     ORDER BY reservation.time_and_date;
 END
